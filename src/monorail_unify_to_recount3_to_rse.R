0a1a5bcfa93dc585f7db1bdbba51bf66bb74b394
David McGaughey <mcgaughey@gmail.com>
2021-12-25 11:27:13 -0500

    path fixes

--- src/monorail_unify_to_recount3_to_rse.R
+++ src/monorail_unify_to_recount3_to_rse.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 library(recount3)
 library(DESeq2)
-options(recount3_url = "/Users/mcgaugheyd/git/metaRPE/recount3/RSE/")
+options(recount3_url = '~/data/metaRPE_RSE/')
 
 hp <- available_projects()
 hp
@@ -10,7 +10,7 @@ hp
 # Create RSE
 ## Also get the counts returned because the `raw_counts` matrix is  the sum of the coverage across a feature
 ## load this custom create_rse script which fixes a little bug I found involving a mistmatched GTF and count matrix
-source('../src/create_rse_manual.R')
+source('src/create_rse_manual.R')
 rse_list <- list()
 # t
 for (i in hp$project){
@@ -26,8 +26,8 @@ rse_gene <- do.call(cbind, rse_list)
 
 # Import in my metadata and add to colData
 
-lane_info <- read_tsv('../data/ogvfb_working_meta_seq_lane.tsv')
-meta_info <- read_tsv('../data/ogvfb_full_meta.tsv')
+lane_info <- read_tsv('data/ogvfb_working_meta_seq_lane.tsv')
+meta_info <- read_tsv('data/ogvfb_full_meta.tsv')
 info <- lane_info %>% 
   mutate(Class = case_when(is.na(Class) ~ 'Development', 
                            TRUE ~ Class)) %>% 
